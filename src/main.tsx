54c162cf2d907fe864eaba791f8a1ad2e5bcd232
francisamidu <francisamidu124@gmail.com>
2023-02-03 17:47:14 +0200

    chore:Changed project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@fontsource/inter';
 import App from './App';
 
 const rootElement = document.getElementById('root') as HTMLElement;
