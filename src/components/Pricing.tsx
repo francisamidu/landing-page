a1819d2413c66325b04b3fd5abf13337552ad505
francisamidu <francisamidu124@gmail.com>
2023-02-04 09:04:48 +0200

    UI tweaks

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -14,20 +14,22 @@ const Pricing = () => {
         Create stunning works of art without paying $100 for a graphic designer
       </h2>
 
-      <div className="mt-4 grid grid-cols-1 place-content-center md:grid-cols-3">
+      <div className="mt-4 grid grid-cols-1 place-content-center gap-2 md:grid-cols-3">
         {pricing.map((p, i) => (
           <div
-            className="flex flex-col rounded-md border-[1px] border-slate-800 p-5"
+            className="flex flex-col items-stretch rounded-md border-[1px] border-slate-800 p-5"
             key={i}
           >
-            <span className="my-3 rounded-3xl bg-slate-500 p-3 font-bold text-slate-800">
+            <span className="my-4 w-fit rounded-3xl bg-slate-500 p-1 text-sm font-bold uppercase text-slate-800">
               {p.cost}
             </span>
-            <span className="mb-4 text-2xl text-white">{p.type}</span>
+            <span className="mb-4 text-center text-2xl font-bold text-white">
+              {p.type}
+            </span>
             {p.benefits.map((b) => (
               <ul className="flex flex-col p-0" key={JSON.stringify(b)}>
-                <li className="flex flex-row items-center">
-                  <CheckIcon className="mr-3 text-purplish-500" size={22} />
+                <li className="mb-3 flex flex-row items-center">
+                  <CheckIcon className="mr-3 !text-slate-500" size={22} />
                   <span className="text-slate-500">{b}</span>
                 </li>
               </ul>
