b3a6f9e6966ea1eebbf52f2e7e08f64e89095f45
francisamidu <francisamidu124@gmail.com>
2023-02-04 10:45:58 +0200

    UI tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
   const columns = shared.columns;
   return (
     <footer className="grid grid-cols-4 p-5">
-      <div>
+      <div className="mr-3">
         <h1 className="text-3xl font-bold text-white">{shared.name}</h1>
-        <p className="mt-3 text-slate-400">{shared.description}</p>
+        <p className="mt-5 text-slate-400">{shared.description}</p>
       </div>
       {columns.slice(0, 3).map((c) => (
-        <div key={JSON.stringify(c)}>
+        <div className="place-self-center" key={JSON.stringify(c)}>
           <h2 className="text-xs font-bold uppercase text-slate-400">
             {c.name}
           </h2>
@@ -26,6 +26,18 @@ const Footer = () => {
           </ul>
         </div>
       ))}
+      <div className="flex flex-row items-center justify-center">
+        <p>
+          &copy; {new Date().getFullYear()} {shared.name}
+        </p>
+        <span className="dot"></span>
+        {columns[-1].links.map((c, i) => (
+          <div key={i}>
+            <span className="text-slate-400">{c}</span>
+            <span className="dot"></span>
+          </div>
+        ))}
+      </div>
     </footer>
   );
 };
