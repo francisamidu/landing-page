8dabe4864d5521d8cb1ac7832169474eb37ca347
francisamidu <francisamidu124@gmail.com>
2023-02-04 11:35:18 +0200

    UI tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,17 +5,17 @@ const Footer = () => {
   const columns = shared.columns;
   const lastColumn = columns[columns.length - 1];
   return (
-    <footer className="grid grid-cols-4 py-10">
-      <div className="mr-3">
+    <footer className="flex flex-col p-5 md:grid md:grid-cols-4 md:py-10">
+      <div className="mb-3 md:mr-3">
         <h1 className="text-3xl font-bold text-white">{shared.name}</h1>
         <p className="mt-10 text-slate-400">{shared.description}</p>
       </div>
       {columns.slice(0, 3).map((c) => (
-        <div className="mb-8 place-self-center" key={JSON.stringify(c)}>
+        <div className="mb-8 md:place-self-center" key={JSON.stringify(c)}>
           <h2 className="text-xs font-bold uppercase text-slate-400">
             {c.name}
           </h2>
-          <ul className="mt-8">
+          <ul className="mt-5 md:mt-8">
             {c.links.map((l) => (
               <li
                 key={l}
@@ -27,18 +27,18 @@ const Footer = () => {
           </ul>
         </div>
       ))}
-      <div className="col-start-1 col-end-5 mt-7 flex flex-row items-center justify-center">
+      <div className="col-start-1 col-end-5 mt-7 flex flex-col items-center justify-center md:flex-row">
         <p>
           &copy; {new Date().getFullYear()} {shared.name}
         </p>
-        <span className="dot"></span>
+        <span className="dot hidden md:block"></span>
         {lastColumn.links.map((c, i) => (
           <div className="flex flex-row items-center justify-center" key={i}>
             <a href="#" className="text-slate-400 hover:text-purplish-500">
               {c}
             </a>
             {lastColumn.links.length - i != 1 ? (
-              <span className="dot"></span>
+              <span className="dot hidden md:block"></span>
             ) : null}
           </div>
         ))}
