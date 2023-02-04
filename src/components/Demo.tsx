450fcdd1924b61425b341a455529069b68eacd41
francisamidu <francisamidu124@gmail.com>
2023-02-04 04:28:16 +0200

    UI tweaks

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Stat } from '.';
 import { Play, PlayIcon } from 'lucide-react';
-import image from '../assets/images/powered-by-vitawind-bright.png';
+import image from '../assets/images/dashboard.jpg';
 
 const Demo = () => {
   return (
     <section className="py-10">
-      <div className="max-w-screen-lg flex-col items-center justify-center md:mx-auto">
+      <div className="max-w-screen-md flex-col items-center justify-center md:mx-auto">
         <div
-          className="flex flex-col items-center justify-center"
+          className="h-[450px]"
           style={{
             backgroundImage: `url(${image})`,
             backgroundPosition: 'center',
@@ -16,10 +16,15 @@ const Demo = () => {
             backgroundSize: 'cover',
           }}
         >
-          <button className="flex flex-row items-center justify-center rounded-3xl bg-gray-50 py-3.5 px-5">
-            <PlayIcon className="text-purplish-500" size={18} />
-            <span className="text-md text-slate-800">Watch quick demo</span>
-          </button>
+          <div className="demo flex  flex-col items-center justify-center">
+            <button
+              className="flex flex-row items-center justify-center rounded-3xl bg-gray-50 py-3.5 px-5 shadow"
+              type="button"
+            >
+              <PlayIcon color="#574bc5" size={18} />
+              <span className="text-md text-slate-800">Watch quick demo</span>
+            </button>
+          </div>
         </div>
         <Stat />
       </div>
