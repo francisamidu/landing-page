e3c039348238a558487bffe0326249b906690e23
francisamidu <francisamidu124@gmail.com>
2023-02-04 12:19:16 +0200

    updated README

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from 'lucide-react';
 const Hero = () => {
   return (
     <section className="my-28 flex flex-col items-center justify-center text-center">
-      <h1 className="px-4 text-3xl font-bold capitalize md:w-3/5 md:px-0 md:text-6xl md:leading-[4rem]">
+      <h1 className="px-4 text-4xl font-bold capitalize md:w-3/5 md:px-0 md:text-6xl md:leading-[4rem]">
         The Future of <span className="text-purplish-500">Creative Art</span>{' '}
         reimagined with AI Image generation
       </h1>
