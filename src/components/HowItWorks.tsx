9d37b11b056755b3787eee5ea70d949e7eecf7e4
francisamidu <francisamidu124@gmail.com>
2023-02-04 07:01:18 +0200

    feat:Added FAQs

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -6,7 +6,7 @@ import Step from './Step';
 const HowItWorks = () => {
   return (
     <section className="py-5">
-      <div className="flex max-w-screen-lg flex-col md:mx-auto">
+      <div className="flex max-w-screen-xl flex-col md:mx-auto">
         <div className="my-3 text-center">
           <h1 className="mb-3 text-xs font-bold uppercase">
             How to generate Ai images
@@ -14,7 +14,7 @@ const HowItWorks = () => {
           <h2 className="text-3xl font-bold">How it all works</h2>
         </div>
         <div className="flex flex-row justify-center">
-          <div className="mr-3 flex w-2/5 flex-col items-center justify-center">
+          <div className="mr-3 flex w-[55%] flex-col items-center justify-center">
             <video
               src={video}
               autoPlay={false}
@@ -22,7 +22,7 @@ const HowItWorks = () => {
               className="h-auto w-full"
             ></video>
           </div>
-          <div className=" ml-3 w-3/5  flex-col items-center">
+          <div className="ml-3 w-[45%]  flex-col items-center">
             {shared.steps.map((step, index) => (
               <Step key={JSON.stringify(step)} index={index} text={step} />
             ))}
--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Step = ({ index, text }: StepProps) => {
   return (
-    <div className="my-3 flex w-fit flex-row rounded-3xl bg-transluscent-100 py-3 px-5 transition-all duration-300 hover:scale-125 hover:cursor-pointer">
+    <div className="my-3 flex w-fit flex-row rounded-3xl bg-transluscent-100 py-3 px-5 transition-all duration-300 hover:scale-105 hover:cursor-pointer">
       <span className="mr-3 text-purplish-500">{index + 1}</span>
       <p>{text}</p>
     </div>
