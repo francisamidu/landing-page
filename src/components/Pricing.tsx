83f97d26018bd901733f12364e678df773f7a3a3
francisamidu <francisamidu124@gmail.com>
2023-02-04 09:30:14 +0200

    UI tweaks

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -5,29 +5,38 @@ import { MainButton } from '.';
 
 const Pricing = () => {
   const pricing = shared.pricing;
+  const Cost = (cost: string) => (
+    <>
+      <span className="text-sm">$</span>
+      {cost}
+    </>
+  );
   return (
     <section className="py-5">
       <h1 className="text-center text-2xl font-bold capitalize">
         Simple, transparent pricing
       </h1>
-      <h2 className="text-center">
+      <h2 className="text-center text-slate-500">
         Create stunning works of art without paying $100 for a graphic designer
       </h2>
 
       <div className="mt-10 grid grid-cols-1 place-content-center gap-2 md:grid-cols-3">
         {pricing.map((p, i) => (
           <div
-            className="flex flex-1 flex-col items-stretch justify-center rounded-md border-[1px] border-slate-800 p-5"
+            className={`
+            flex flex-1 flex-col justify-between rounded-md border-[1px] border-slate-900 p-5
+            ${i == 1 ? 'scale-105 bg-slate-800' : ''}
+            `}
             key={i}
           >
             <div className="grid place-items-center">
-              <span className="my-4 w-fit rounded-3xl bg-slate-500 p-1 px-3 text-xs font-bold uppercase text-slate-800">
+              <span className="my-4 w-fit rounded-3xl bg-slate-800 p-1 px-3 text-xs font-bold uppercase text-white">
                 {p.type}
               </span>
             </div>
-            <span className="mb-4 text-center text-2xl font-bold text-white">
-              {`${Number(p.cost) ? `$${p.cost}` : p.cost}`}
-            </span>
+            <p className="mb-4 text-center text-3xl font-bold text-white">
+              {Number(p.cost) ? Cost(p.cost) : p.cost}
+            </p>
             {p.benefits.map((b) => (
               <ul className="flex flex-col p-0" key={JSON.stringify(b)}>
                 <li className="mb-3 flex flex-row items-center">
