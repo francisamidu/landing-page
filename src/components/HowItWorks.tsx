35cf7c91d57480bbec5afb8dc99472adc3a637af
francisamidu <francisamidu124@gmail.com>
2023-02-04 06:30:13 +0200

    UI tweaks

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -6,25 +6,27 @@ import Step from './Step';
 const HowItWorks = () => {
   return (
     <section className="py-5">
-      <div className="flex max-w-screen-lg flex-col items-center md:mx-auto">
+      <div className="flex max-w-screen-lg flex-col md:mx-auto">
         <div className="my-3 text-center">
           <h1 className="mb-3 text-xs font-bold uppercase">
             How to generate Ai images
           </h1>
           <h2 className="text-3xl font-bold">How it all works</h2>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <video
-            src={video}
-            autoPlay={false}
-            controls
-            className="my-5 h-auto w-3/5"
-          ></video>
-        </div>
-        <div className="mt-3">
-          {shared.steps.map((step, index) => (
-            <Step key={JSON.stringify(step)} index={index} text={step} />
-          ))}
+        <div className="flex flex-row justify-center">
+          <div className="mr-3 flex w-2/5 flex-col items-center justify-center">
+            <video
+              src={video}
+              autoPlay={false}
+              controls
+              className="h-auto w-full"
+            ></video>
+          </div>
+          <div className=" ml-3 w-3/5  flex-col items-center">
+            {shared.steps.map((step, index) => (
+              <Step key={JSON.stringify(step)} index={index} text={step} />
+            ))}
+          </div>
         </div>
       </div>
     </section>
